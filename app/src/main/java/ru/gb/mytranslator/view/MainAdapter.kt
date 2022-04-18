8b017f41268779e6b5537b7a6d9a0f1fd5153450
pfrost982 <pfrost982@gmail.com>
2022-04-18 23:01:22 +0700

    добавил делегат ViewByIdDelegate

--- app/src/main/java/ru/gb/mytranslator/view/MainAdapter.kt
+++ app/src/main/java/ru/gb/mytranslator/view/MainAdapter.kt
@@ -3,10 +3,11 @@ package ru.gb.mytranslator.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import geekbrains.ru.translator.R
-import kotlinx.android.synthetic.main.activity_main_recyclerview_item.view.*
 import ru.gb.data.DataModel
+import kotlin.reflect.KProperty
 
 class MainAdapter(
     val onItemClick: (itemData: DataModel) -> Unit,
@@ -37,12 +38,21 @@ class MainAdapter(
     inner class RecyclerItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(data: DataModel) {
+
+            val header by ViewByIdDelegate<TextView>(R.id.header_textview_recycler_item, itemView)
+            val description by ViewByIdDelegate<TextView>(R.id.description_textview_recycler_item, itemView)
+
             if (layoutPosition != RecyclerView.NO_POSITION) {
-                itemView.headerTextviewRecyclerItem.text = data.text
-                itemView.descriptionTextviewRecyclerItem.text =
-                    data.meanings?.get(0)?.translation?.translation
+                header.text = data.text
+                description.text = data.meanings?.get(0)?.translation?.translation
                 itemView.setOnClickListener { onItemClick(data) }
             }
         }
+    }
+}
+
+class ViewByIdDelegate<out T : View?>(private val viewId: Int, private val itemView: View) {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return itemView.findViewById<T>(viewId)
     }
 }
